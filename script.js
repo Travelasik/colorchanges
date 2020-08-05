a3e294b56801006f7ec6f8e89c340da12ea7cb6d
Travelasik <67265798+Travelasik@users.noreply.github.com>
2020-08-05 21:15:34 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+let name = "button";
+console.log(name);
+name = "body";
+
+const RandomNumber= () => {
+    Math.floor(Math.random() * 10);
+};	
+
+const randomColor= () => {
+    
+}
+
+
+button.addEventListener('click', RandomColor)
